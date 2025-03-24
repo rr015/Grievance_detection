91d30d9fd12b904d3e29639d41463d8a39a47fc9
rr015 <140845576+rr015@users.noreply.github.com>
2025-03-24 19:58:08 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,14 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC_5gTibwSs34m-AXyriISqgTpePUBhB3k",
-  authDomain: "pollution-management.firebaseapp.com",
-  databaseURL: "https://pollution-management-default-rtdb.firebaseio.com",
-  projectId: "pollution-management",
-  storageBucket: "pollution-management.firebasestorage.app",
-  messagingSenderId: "316043379968",
-  appId: "1:316043379968:web:39d1330ed73d20e9ca9fe5",
-  measurementId: "G-H0DT3CY5H2"
+  apiKey: ,
+  authDomain: ,
+  databaseURL: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ,
+  appId: ,
+  measurementId: 
 };
 
 // Initialize Firebase
